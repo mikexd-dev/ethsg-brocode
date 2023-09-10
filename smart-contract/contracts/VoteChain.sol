e960163cafd717523c8ac59e57f726641298a682
oxrajesh <41378186+oxrajesh@users.noreply.github.com>
2023-09-10 23:11:37 +0800

    added createProposal & TransferHelper contract

--- smart-contract/contracts/VoteChain.sol
+++ smart-contract/contracts/VoteChain.sol
@@ -11,8 +11,29 @@ contract VoteChain is Ownable {
     // Mapping of Donors & Status
     mapping(address => bool) public donorList;
 
+    // Mapping First Donatied
+    mapping(address => bool) public firstDonation;
+
     // Mapping of Donation categories
     mapping(bytes32 => uint256) public donationCategory;
+
+    // Mapping of Proposals
+    mapping(bytes32 => bool) public proposals;
+
+    // Mapping of Approved Proposals
+    mapping(bytes32 => bool) public approvedProposals;
+
+    // Total No.of Proposals
+    uint256 public totalProposals;
+
+    // Mapping of Proposals & Vendors
+    mapping(bytes32 => address[]) public vendorWallets;
+
+    // Mapping of Proposals & Amounts
+    mapping(bytes32 => uint256[]) public vendorAmounts;
+
+    // Mapping of Vendor Invoices
+    mapping(bytes32 => string) public vendorInvoices;
 
     // Events
     event NPOUpdated(address indexed npo, bool status);
@@ -22,7 +43,16 @@ contract VoteChain is Ownable {
         string category,
         address token,
         uint256 amount,
-        uint256 when
+        uint256 createdAt
+    );
+    event ProposalCreated(
+        bytes32 proposalId,
+        address indexed createdBy,
+        string category,
+        address[] vendors,
+        uint256[] amounts,
+        string invoice,
+        uint256 createdAt
     );
 
     // Update NPO Wallet Status
@@ -70,6 +100,49 @@ contract VoteChain is Ownable {
             amount
         );
         updateCategoryDonation(category, amount);
+        if (!firstDonation[msg.sender]) {
+            firstDonation[msg.sender] = true;
+        }
         emit Donated(msg.sender, category, token, amount, block.timestamp);
+    }
+
+    // Check if the given wallet is npo or not
+    function getNPOStatus(address npo) public view returns (bool) {
+        return npoList[npo];
+    }
+
+    // Convert Number to Keccak256
+    function getProposalId(
+        uint256 number
+    ) public pure returns (bytes32 result) {
+        result = keccak256(abi.encodePacked(number));
+    }
+
+    // Create Proposal
+    function createProposal(
+        string memory category,
+        address[] memory vendors,
+        uint256[] memory amounts,
+        string memory invoices
+    ) external {
+        require(getNPOStatus(msg.sender), "CreateProposal:: Should be NPO");
+        bytes32 proposalId = getProposalId(totalProposals);
+        require(
+            vendors.length == amounts.length,
+            "CreateProposal:: Vendors & amounts count should be equal"
+        );
+        vendorWallets[proposalId] = vendors;
+        vendorAmounts[proposalId] = amounts;
+        vendorInvoices[proposalId] = invoices;
+        totalProposals++;
+        emit ProposalCreated(
+            proposalId,
+            msg.sender,
+            category,
+            vendors,
+            amounts,
+            invoices,
+            block.timestamp
+        );
     }
 }
