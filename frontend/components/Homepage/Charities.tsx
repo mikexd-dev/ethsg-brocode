5b3d92a112b0adac3711df9fe7b7f1ea9bff5a7d
Mike Zhang <mikexd.dev@gmail.com>
2023-09-11 19:10:19 +0800

    fix error

--- frontend/components/Homepage/Charities.tsx
+++ frontend/components/Homepage/Charities.tsx
@@ -137,43 +137,57 @@ const Charities = ({ npo }: any) => {
     },
   ];
 
-    const [showPopup, setShowPopup] = useState(false)
-    const [selectedCharity, setSelectedCharity] = useState({})
+  const [showPopup, setShowPopup] = useState(false);
+  const [selectedCharity, setSelectedCharity] = useState({});
 
-    const showCharity = (index: number) => {
-        setSelectedCharity(CharitiesArray[index])
-        setShowPopup(true)
-    }
+  const showCharity = (index: number) => {
+    setSelectedCharity(CharitiesArray[index]);
+    setShowPopup(true);
+  };
 
-    return <>
-        <div className="w-full mt-12">
-            <h1 className="text-3xl font-semibold mb-4">Charities</h1>
+  return (
+    <>
+      <div className="w-full mt-12">
+        <h1 className="text-3xl font-semibold mb-4">Charities</h1>
 
-            <div className="grid grid-cols-3 gap-4 w-full">
-                {
-                    CharitiesArray.map((charity, index) => {
-                        return <div key={index} onClick={() => showCharity(index)} className="bg-white rounded-lg p-4 w-full cursor-pointer">
-                            <p className="flex justify-between font-semibold">
-                                <img className="w-[60px] h-[60px] object-cover rounded-full" src={charity.image}></img>
-                                <span className="tag tagBlue">Family</span>
-                            </p>
-                            <p className="text-slate-800 mt-2 font-semibold">{charity.npo}</p>
-                            <div className="mt-12">
-                                <hr />
-                                <div className="mt-2 flex justify-between">
-                                    <p className="text-gray-400 text-sm uppercase font-semibold">Proposals</p>
-                                    <p className="text-sm">1 in progress | 3 passed</p>
-                                </div>
-                            </div>
-                        </div>
-                    })
-                }
-            </div>
+        <div className="grid grid-cols-3 gap-4 w-full">
+          {CharitiesArray.map((charity, index) => {
+            return (
+              <div
+                key={index}
+                onClick={() => showCharity(index)}
+                className="bg-white rounded-lg p-4 w-full cursor-pointer"
+              >
+                <p className="flex justify-between font-semibold">
+                  <img
+                    className="w-[60px] h-[60px] object-cover rounded-full"
+                    src={charity.image}
+                  ></img>
+                  <span className="tag tagBlue">Family</span>
+                </p>
+                <p className="text-slate-800 mt-2 font-semibold">
+                  {charity.npo}
+                </p>
+                <div className="mt-12">
+                  <hr />
+                  <div className="mt-2 flex justify-between">
+                    <p className="text-gray-400 text-sm uppercase font-semibold">
+                      Proposals
+                    </p>
+                    <p className="text-sm">1 in progress | 3 passed</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
+      </div>
 
-        {
-            showPopup && selectedCharity ? <CharityPopup charity={selectedCharity} /> : <></>
-        }
+      {showPopup && selectedCharity ? (
+        <CharityPopup charity={selectedCharity} />
+      ) : (
+        <></>
+      )}
     </>
   );
 };
